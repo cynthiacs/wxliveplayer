5addfb0e6fec81c5dba881abfc689df60f69974b
shu chen <lucky_chsh@126.com>
2018-06-21 15:28:19 +0800

    liveplayer init

---
+++ pages/index/index.js
@@ -0,0 +1,49 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+var types = ['default', 'primary', 'warn']
+var pageObject = {
+  data: {
+    defaultSize: 'default',
+    primarySize: 'default',
+    warnSize: 'default',
+    disabled: false,
+    plain: false,
+    loading: false
+  },
+  setDisabled: function (e) {
+    this.setData({
+      disabled: !this.data.disabled
+    })
+  },
+  setPlain: function (e) {
+    this.setData({
+      plain: !this.data.plain
+    })
+  },
+  setLoading: function (e) {
+    this.setData({
+      loading: !this.data.loading
+    })
+  },
+  openWebview: function (e) {
+    wx.navigateTo({
+      url: '../webview/webview',
+    })
+  }
+}
+
+for (var i = 0; i < types.length; ++i) {
+  (function (type) {
+    pageObject[type] = function (e) {
+      var key = type + 'Size'
+      var changedData = {}
+      changedData[key] =
+        this.data[key] === 'default' ? 'mini' : 'default'
+      this.setData(changedData)
+    }
+  })(types[i])
+}
+
+Page(pageObject)
